7f341b4a724fb86ba2fbd9d9dfec06008c7729bb
QuantumAnalyticsSport <quantum.sport.analytics@gmail.com>
2025-06-27 18:45:54 +0200

    Update table.py

--- table.py
+++ table.py
@@ -108,3 +108,35 @@ def draw_table_frame(speeds, shot_classif, player_info, frame_idx, fps=25,
     img = np.asarray(canvas.get_renderer().buffer_rgba())[..., :3].copy()
     plt.close(fig)
     return img, new_max_state, new_shot_state
+
+
+
+
+def create_table_video(bbox_dico, shot_classif, player_info, output_path="table_stats_styled.mp4", fps=25):
+    speeds = compute_speeds(bbox_dico, fps)
+    all_frames = sorted(set.intersection(*(set(speeds[p].keys()) for p in player_info)))
+
+    last_max_state = {}
+    last_max_state_ = {}
+    last_shot_state = {}
+    sample_img, _, _ = draw_table_frame(speeds, shot_classif, player_info, all_frames[0], fps,
+                                        last_max_state=last_max_state,
+                                        last_max_state_ = last_max_state_,
+                                        last_shot_state=last_shot_state)
+    
+    h, w, _ = sample_img.shape
+    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+
+    for frame in all_frames:
+        frame_img, last_max_state, last_shot_state = draw_table_frame(
+            speeds, shot_classif, player_info, frame, fps,
+            last_max_state=last_max_state,
+            last_shot_state=last_shot_state
+        )
+
+        frame_bgr = cv2.cvtColor(frame_img, cv2.COLOR_RGB2BGR)
+        out.write(frame_bgr)
+
+    out.release()
+    print(f"Saved with max transitions: {output_path}")
+
